b985bb0fb623bc689982d0084661440e32708f16
Fabio Kreusch <fabiokr@gmail.com>
2018-01-25 09:53:35 -0200

    Use scss to process css

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem "money"
 gem "sqlite3"
+gem "sass"
--- lib/invest/html.rb
+++ lib/invest/html.rb
@@ -1,10 +1,13 @@
 require "erb"
 require "money"
+require "sass"
 
 I18n.enforce_available_locales = false
 
 class Invest
   class Html
+    TEMPLATES_PATH = "lib/invest/templates/html/"
+
     attr_reader :invest
 
     # Public: Initializes the Html class.
@@ -21,7 +24,17 @@ class Invest
     # Returns nothing.
     def save!(file)
       File.write(file, ERB.new(
-        File.read("lib/invest/templates/report.html.erb")).result(binding))
+        File.read(File.join(TEMPLATES_PATH, "report.html.erb"))).result(binding))
+    end
+
+    # Public: Outputs the css.
+    #
+    # Returns a String.
+    def css
+      Sass::Engine.new(
+        File.read(File.join(TEMPLATES_PATH, "report.scss")),
+        syntax: :scss
+      ).render
     end
   end
 end
